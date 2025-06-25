030ca0ad83b855ca3a90641fd72140f4a5936961
vemarx <vemarx.dev@gmail.com>
2025-06-25 09:57:13 -0300

    atualizações na login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
-        <title>FindenHub</title>
+        <title>{{ config( 'app.name', 'FindenHub')}}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
@@ -14,13 +14,13 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased text-[#1e2a38]">
-        <div class="min-h-screen bg-gray-100">
+    <body class="font-sans antialiased text-[#1E2A38]">
+        <div class="min-h-screen bg-[#E6EDF4]">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             @isset($header)
-                <header class="bg-white shadow">
+                <header class="bg-[#F4F6F8] shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
